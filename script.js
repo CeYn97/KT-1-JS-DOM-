81f2351d3b0aa98760af6c87b589f5d3be2329dd
CeYn97 <Kfur4ik@gmail.com>
2025-02-08 22:36:39 +0300

    commit2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const arr = Array.from({ length: 30 }, () => Math.floor(Math.random() * 100)); // Массив 30 случайных чисел от 0 до 99
+const arr = Array.from({ length: 30 }, () => Math.floor(Math.random() * 100));
 let table = document.getElementById("table");
 
 for (let i = 0; i < 5; i++) {
@@ -15,14 +15,12 @@ for (let i = 0; i < 5; i++) {
 function foo() {
   let rows = table.getElementsByTagName("tr");
 
-  let newValue = Math.floor(Math.random() * 100); // Генерация случайного числа от 0 до 99
+  let newValue = Math.floor(Math.random() * 100);
   let colored = newValue >= 50 ? 'style="background-color: orange;"' : "";
 
   if (rows.length === 0 || rows[rows.length - 1].children.length >= 6) {
-    // Если таблица пустая или последняя строка заполнена, добавляем новую строку
     table.innerHTML += `<tr><td ${colored}>${newValue}</td></tr>`;
   } else {
-    // Добавляем ячейку в последнюю строку
     rows[rows.length - 1].innerHTML += `<td ${colored}>${newValue}</td>`;
   }
 }
